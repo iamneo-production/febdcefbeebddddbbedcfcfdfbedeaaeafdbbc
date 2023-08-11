5b3ea121f7a5ca662c87f9794130b0a32a481e6d
Iamneo <techadmin@iamneo.ai>
2023-08-11 06:56:34 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Button from "./components/UI/Button/Button";
+import Card from  "./components/UI/Card/Card";
+import Banner from "./components/UI/Banner/Banner";
 
 function App() {
+  // State variables to control the start, attempt, question, card and result of the Quiz
+  const [start, setStart] = useState(true);
+  const [attempt, correctAnswerMarkUpdate] = useState(0);
+  const [qsnAttempt, setqsnAttempt] = useState(0);
+  const [card, setCard] = useState(false);
+  const [result, setResult] = useState(false)
+  const [banner,setBanner] = useState(false)
+
+  // Event handler to start the Quiz
+  const handleStart = () => {
+    setStart(false)
+    setCard(true)
+    correctAnswerMarkUpdate(0)
+    setqsnAttempt(0)
+    setBanner(false)
+  }
+
+  // Options for the quiz
+  const opt = {
+    option1 : "Blue",option2 : "Red",option3 : "Yellow",option4 : "Green"
+  }
+  // Array of questions
+  const questions = [    { question: "Who is the father of our nation?", answer: "Green" },    { question: "What color is the leaves?", answer: "Green" },    { question: "What color is the sky?", answer: "Blue" },    { question: "What color is the sky?", answer: "Blue" },    { question: "What color is the fire?", answer: "Red" }  ];
+
+  // Event handler to show the result of the Quiz
+  const handleResult = () => {
+    setStart(true)
+    setBanner(true)
+    setCard(false)
+    setResult(false)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center mt-4 ">
+      <div>
+          <div className="flex items-center justify-center space-x-4 mb-12">
+            <h1 className="text-3xl">Quizz App</h1>
+            <i className="">learn react</i>
+          </div>
+              {/* {start && <Button onClick={ handleStart} style={"bg-orange-300 px-10 py-2 rounded hover:bg-orange-400"}>Start Quiz</Button>   } */}
+              {banner && <Banner score={attempt}  />}
+            {card && <div> {questions.map((q, index) => (
+                <Card key={index} question={q.question}  correctAnswerMarkUpdate={correctAnswerMarkUpdate} attempt={attempt} options={opt} answer={q.answer} setQsns={setqsnAttempt} qsn={ qsnAttempt} setCard={setResult} qsnAttempt={qsnAttempt} />            
+              ))}
+          </div>}
+          {result && <Button onClick={handleResult} style={"bg-orange-300 px-10 py-2 rounded hover:bg-orange-400 mt-3"} >Show Results</Button>}
+          {start && <Button onClick={ handleStart} style={"bg-orange-300 px-10 py-2 rounded hover:bg-orange-400"}>Start Quiz</Button>   }
+      </div>
     </div>
   );
 }
---
+++ reactapp/src/QuizComp.js
@@ -0,0 +1,111 @@
+import React from 'react'
+import { useState } from 'react';
+import './quizstyle.css'
+import  Card from "./components/UI/Card/Card.js";
+
+const QuizComp = () => {
+    var Questionbank = [
+        {Card},
+        {
+            Question: "Who is the father of your nation ?",
+            Answers: [
+                { Answer: "Mahatma Gandhi", isCorrect: true },
+                { Answer: "Jawaharlal Nehru", isCorrect: false },
+                { Answer: "Donald Trump", isCorrect: false },
+                { Answer: "Barrack Obama", isCorrect: false }
+            ]
+        }, {
+            Question: "What color is are the leaves ?",
+            Answers: [
+                { Answer: "Blue", isCorrect: false },
+                { Answer: "Red", isCorrect: false },
+                { Answer: "Yellow", isCorrect: false },
+                { Answer: "Green", isCorrect: true }
+            ]
+        },
+        {
+            Question: "What color is the sky ?",
+            Answers: [
+                { Answer: "Blue", isCorrect: true },
+                { Answer: "Red", isCorrect: false },
+                { Answer: "Yellow", isCorrect: false },
+                { Answer: "Green", isCorrect: false }
+            ]
+        },
+        {
+            Question: "What color is the sky ?",
+            Answers: [
+                { Answer: "Blue", isCorrect: true },
+                { Answer: "Red", isCorrect: false },
+                { Answer: "Yellow", isCorrect: false },
+                { Answer: "Green", isCorrect: false }
+            ]
+        }
+    ]
+
+    //useState Hook
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [score, setScore] = useState(0);
+    const [showScore, setShowScore] = useState(false);
+
+const handleAnswerResponse=(isCorrect)=>
+{
+    if(isCorrect)
+    {
+        setScore(score+1);
+    }
+
+   const nextQuestion= currentQuestion+1;
+   if(nextQuestion<Questionbank.length)
+   {
+    setCurrentQuestion(nextQuestion);
+   }
+   else{
+    setShowScore(true);
+   }
+}
+
+const resetQuiz=()=>
+{
+    setCurrentQuestion(0);
+    setScore(0);
+    setShowScore(false);
+}
+
+    return (
+        <div className='app'>
+            {showScore ? (
+                <div className='score-section'>
+                    You have scored {score} out of {Questionbank.length}
+                    <>
+                       <button type="submit" onClick={resetQuiz}>Play Again!!</button>
+                    </>
+                </div>
+            )
+                : (
+                    <>
+                        <div className='question-section'>
+                            <div className='question-count'>
+                               <span>{currentQuestion+1}</span>/{Questionbank.length}
+                            </div>
+
+                            <div className='question-text'>
+                             {Questionbank[currentQuestion].Question}
+                            </div>
+                        </div>
+
+                        <div className='answer-section'>
+                          {Questionbank[currentQuestion].Answers.map((answer)=>
+                          (
+                              <button onClick={()=>handleAnswerResponse(answer.isCorrect)}>{answer.Answer}</button>
+                          ))}
+                        </div>
+                    </>
+                )
+            }
+
+        </div>
+    );
+}
+
+export default QuizComp;
